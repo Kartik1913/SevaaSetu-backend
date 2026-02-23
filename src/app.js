1937872a59cabbbec2bc6106f1cc00088f93abb3
Kartik Shetty <145954755+Kartik1913@users.noreply.github.com>
2026-02-23 18:58:22 +0530

    corn-job(making health route)

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/ngo", require("./routes/ngo.routes"));
 app.use("/api/need", require("./routes/need.routes"));
